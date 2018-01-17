1bbca980a762aa66f22b206be8ee3b0cd75d2194
Tomas Barton <tbarton@ualberta.ca>
2018-01-17 14:46:25 -0700

    First working version. Needs **a lot** of refactoring

--- src/little_r/station.py
+++ src/little_r/station.py
@@ -1,6 +1,16 @@
-from .record import Record
-import arrow
+"""station.py
+
+Author: Tomas Barton, tommz9@gmail.com
+
+"""
 import json
+import csv
+import os
+
+import arrow
+
+from .record import Record
+
 
 class Station:
     """A factory method to create records for one station.
@@ -8,16 +18,31 @@ class Station:
     Holds the information about the station name, location and height.
     """
 
-    def __init__(self, name, lat, lon, height, timezone=None):
+    def __init__(self, name, lat, lon, height, data_file=None, timezone=None):
+        """Create the station object."""
 
         self.name = name
         self.lat = lat
         self.lon = lon
-        self.height =  height
+        self.height = height
+        self.data_file = data_file
         self.timezone = timezone
-        
-    def generate_record(self, data_dictionaries):
 
+    def generate_record(self, data_dictionaries, group_by):
+        """Convert the measurements to records.
+
+        data_dictionaries is a list of dictionaries where each dictionary holds
+        measurements taken in one time.
+
+        group_by is a function that takes the time of the measurement and
+        converts it to a string representing the time part of the filename for 
+        the little_r file. This function can return the same value for several
+        measurements (typically measurements within one hour). These
+        measurement will be saved under one key in the returned dict.
+
+        The function returns a dictionary of lists with measurements. The key
+        of the dictionary is the value returned by the group_by function.
+        """
         result = {}
 
         for one_measurement in data_dictionaries:
@@ -25,7 +50,7 @@ class Station:
 
             if isinstance(time, str):
                 if self.timezone:
-                    time = arrow.get(time, self.timezone)
+                    time = arrow.get(time).shift(hours=6) # TODO: fix utc conversion
                 else:
                     time = arrow.get(time)
 
@@ -33,23 +58,47 @@ class Station:
 
             del one_measurement['datetime']
 
+            one_measurement = {k: float(v) for k, v in one_measurement.items()}
+
             record.merge(one_measurement)
 
-            result[time] = record.little_r_report()
-        
+            key = group_by(time)
+            
+            if key == '2016-04-01_00':
+                break
+
+            record_string = record.little_r_report()
+
+            try:
+                result[key].append(record_string)
+            except KeyError:
+                result[key] = [record_string]
+
         return result
+
+    def generate_record_from_data_file(self, group_by, data_file_argument=None):
+
+        if data_file_argument:
+            self.data_file = data_file_argument
+        
+        with open(self.data_file) as f:
+            reader = csv.DictReader(f)
+            dictionaries = list(reader)
+
+        return self.generate_record(dictionaries, group_by)
 
     @staticmethod
     def create_from_metadata(filename):
-        
+        """Create a station object based on the configuration in json file."""
         with open(filename, 'r') as f:
             metadata = json.load(f)
         
         station = Station(
-            metadata['name'], 
-            metadata['lat'], 
-            metadata['lon'], 
+            metadata['name'],
+            metadata['lat'],
+            metadata['lon'],
             metadata['height'],
+            data_file=os.path.dirname(filename) + '/' + metadata.get('data_file'),
             timezone=metadata.get('timezone'))
-    
+
         return station
--- src/little_r/station_set.py
+++ src/little_r/station_set.py
@@ -1,3 +1,73 @@
+import glob
+import logging
+import sys
+
+from .station import Station
 
 class StationSet:
+    def __init__(self, folder):
+        self.folder = folder
+
+        self.stations = []
+        self.reports = []
+
+        self.logger = logging.getLogger('Station set')
+
+    def discover_stations(self):
+        
+        json_files = glob.glob(self.folder + '/*.json')
+
+        if not json_files:
+            self.logger.info('Cannot find any json files in %s', self.folder)
+
+        for json_file in json_files:
+
+            try:
+                station = Station.create_from_metadata(json_file)
+            except KeyError:
+                self.logger.debug('Cannot process file %s', json_file)
+                continue
+
+            self.logger.info('Found station in %s', json_file)
+
+            self.stations.append(station)
     
+    def generate_reports(self):
+
+        self.reports = []
+
+        for station in self.stations:
+            self.reports.append(station.generate_record_from_data_file(
+                lambda x: x.format('YYYY-MM-DD_HH')))
+
+    def generate_files(self, output_directory, prefix):
+        
+        intervals = self.reports[0].keys()
+
+        for interval in intervals:
+            fn = output_directory + '/obs:' + interval
+            with open(fn, "w") as output_file:
+                for report in self.reports:
+                    try:
+                        output_file.writelines(report[interval])
+                    except KeyError:
+                        pass
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    if len(sys.argv) != 2:
+        logging.error('Folder missing')
+        sys.exit(1)
+
+    folder = sys.argv[1]
+
+    station_set = StationSet(folder)
+
+    station_set.discover_stations()
+    station_set.generate_reports()
+
+    station_set.generate_files(folder, 'obs')
+
